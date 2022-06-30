31609c6d695427d8f9148862c0182e08afc95358
王硕 <519276437@qq.com>
2022-06-30 12:30:33 +0800

    init: 初始化项目

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1 align="center" dir="auto"><a id="user-content-unocss" class="anchor" aria-hidden="true" href="#unocss"><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"></a>VueSureAdmin</h1>
+
+<div  align="center">
+<a href="https://www.pnpm.cn/">
+<img src="https://img.shields.io/badge/pnpm-7.4.0-orange" alt="pnpm"></a><a href="https://v3.cn.vuejs.org/"><img src="https://img.shields.io/badge/vue-3.2.25-brightgreen" alt="vue3"></a><a href="https://nodejs.org/zh-cn/"><img src="https://img.shields.io/badge/node-%3E%3D14.18.0-brightgreen" alt="node"></a>
+</div>
+
+---
+
+## 功能
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: SHUO
+ * @Date: 2022-06-30 11:05:33
+ * @LastEditors: SHUO
+ * @LastEditTime: 2022-06-30 11:24:35
+ * @FilePath: /vue-sure-admin/src/App.vue
+ * @Description: 
+ * 
+-->
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
